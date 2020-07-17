9c65caa4cd377e5a73cdcbb39e37b597fdd3bd23
Roh Woohyeon <roh.woohyeon@gmail.com>
2020-07-18 00:42:59 +0900

    Uninstall js-yaml

--- lib/renderMarkdown.tsx
+++ lib/renderMarkdown.tsx
@@ -5,27 +5,16 @@ import rehypeHighlight from 'rehype-highlight'
 import rehypeRaw from 'rehype-raw'
 import rehypeStringify from 'rehype-stringify'
 import frontmatter from 'remark-frontmatter'
-import jsYaml from 'js-yaml'
 
 export const processor = unified()
   .use(remarkParse)
   .use(frontmatter, ['yaml'])
-  .use(remarkYamlParse)
   .use(remarkRehype, {
     allowDangerousHtml: true
   })
   .use(rehypeRaw)
   .use(rehypeHighlight)
   .use(rehypeStringify)
-
-// transformer
-function remarkYamlParse() {
-  return function (node, file) {
-    if (node.children[0] && node.children[0].type === 'yaml') {
-      file.data.yaml = jsYaml.load(node.children[0].value)
-    }
-  }
-}
 
 export function renderMarkdown(input) {
   return processor.processSync(input).toString()
