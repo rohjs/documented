e97aefcc94fb02a7db960bd7ea46d44530acb906
Roh Woohyeon <roh.woohyeon@gmail.com>
2020-10-31 09:33:48 +0900

    Update introduction

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,17 +8,17 @@ const Index = (): JSX.Element => (
       <section className='section'>
         <h1 className='srOnly'>About</h1>
         <p>
-          Hi, I’m Woohyeon, a frontend developer based in Seoul, Korea and yes,
-          I love designing and building digital products. For past 3+ years,
+          Hi. I’m Woohyeon, a frontend developer based in Seoul, Korea and yes,
+          I love designing and building digital products. For the past 3+ years,
           I've done working in startups, and collaborated with talented people
           to create digital products for both business and consumer use.
           <br />
           I’m a fast learner, prefer to keep learning and continue challenging
-          myself. I like to develop expertise in a number of areas over the
-          course of my life and career. I currently have a full-time commitment
-          at Presto Labs. Outside of my commitments I make online programming
-          courses and run a youtube channel called Kimbug. Feel free to get in
-          touch by any means of communication.
+          myself. I like to develop expertise in several areas throughout my
+          life and career. I currently have a full-time commitment to Presto
+          Labs. Outside of my commitments I make online programming courses and
+          run a youtube channel called Kimbug. Feel free to get in touch by any
+          means of communication.
         </p>
 
         <ul>
